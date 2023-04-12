c4b2bb094155f8a5c8d40518296536bb4b3bd7a6
Kornél Tóth <tkornel23@gmail.com>
2023-04-12 21:14:45 +0200

    done

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,9 +1,7 @@
 import { DateTime } from "luxon"
 
-const API_KEY = 'a5c61c96353a0375fdcaee3e1acea225';
+const API_KEY = 'YOUR API KEY HERE';
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
-
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
