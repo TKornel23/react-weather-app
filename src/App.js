4344a220454ffce585ff7d5a1eb4ab9226414d12
Kornél Tóth <tkornel23@gmail.com>
2023-04-12 18:31:30 +0200

    UI done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeandLocation from './components/TimeandLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getWeatherData from './services/weatherService'
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData('weather', { q: 'london' });
+    console.log(data);
+  }
+
+  fetchWeather();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-400'>
+      <TopButtons />
+      <Inputs />
+
+      <TimeandLocation />
+      <TemperatureAndDetails />
+      <Forecast title={'hourly forecast'}/>
+      <Forecast title={'daily forecast'}/>
     </div>
   );
 }
---
+++ src/services/weatherService.js
@@ -0,0 +1,40 @@
+const API_KEY = 'a5c61c96353a0375fdcaee3e1acea225';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL +  '/' + infoType);
+    url.search = new URLSearchParams({...searchParams, appid:API_KEY})
+
+    return fetch(url)
+        .then((res) => res.json())
+};
+
+const formatCurrentWeather = (data) => {
+    const { 
+        coord: { lat, lon },
+        main: { temp, feels_like, temp_min, temp_max, humidity },
+        name,
+        dt,
+        sys: { country, sunrise, sunset },
+        weather,
+        wind: { speed }
+     } = data
+
+     const { main: details, icon } = weather[0];
+
+     return { 
+        lat, lon, temp,
+        feels_like, temp_min, temp_max,
+        humidity, name, dt,
+        country, sunrise, sunset,
+        details, icon, weather, speed }
+}
+
+const getFormattedWeatherData = (searchParams) => {
+    const formattedCurrentWeather = getWeatherData('weather', searchParams)
+    .then(formatCurrentWeather)
+
+    return formattedCurrentWeather
+}
+
+export default getFormattedWeatherData;
